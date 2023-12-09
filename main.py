7c9afb5d17b3d74c5c4019a99e9cf247adb442cc
Dimitri Shukla <dimitri@dimitri.wifi.local.cmu.edu>
2023-12-09 18:28:19 -0500

    Made function to find targets during surveillance

--- main.py
+++ main.py
@@ -5,7 +5,30 @@ from pyfirmata import Arduino, util
 import time
 
 stateMachine = StateMachine.StateMachine()
+
 seenTargets = set()
+
+def checkForTargets(frame):
+    targets, scores = stateMachine.object_detection.get_target_scores(frame)
+    targets_minHeap = heapify([])
+    offset_x, offset_y = 0, 0
+    for i in range(len(targets)):
+        if scores[i] >= 0:
+            heappush(targets_minHeap, (-1*scores[i], targets[i]))
+    for elem in targets_minHeap:
+        coordinates = stateMachine.object_detection.get_bbox_centroid(elem[1])
+        if (stateMachine.state["frameIndex"], coordinates[0], coordinates[1]) in seenTargets:
+            continue
+        rows, cols, _ = frame.shape
+        cx, cy = int(cols/2), int(rows/2)
+        angle_x = stepper.track_base_stepper(coordinates[0]-offset_x, cx, stateMachine.board)
+        angle_y = stepper.track_camera_stepper(coordinates[1]-offset_y, cy, stateMachine.board)
+        stateMachine.state["base_angle"] += angle_x
+        stateMachine.state["camera_angle"] += angle_y
+        offset_x, offset_y = coordinates[0]-cx, coordinates[1]-cy
+        # pump.shoot_liquid(stateMachine.board)
+        seenTargets.add((stateMachine.state["frameIndex"],coordinates[0], coordinates[1]))
+
 
 def surveillance():
     for i in range(100):
@@ -13,6 +36,7 @@ def surveillance():
             break
         stateMachine.surveillance_camera()
         frame = stateMachine.camera.capture_array()
+        checkForTargets(frame)
     stateMachine.surveillance_base()
     time.sleep(1)
     
