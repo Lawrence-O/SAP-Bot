32ab9a32895222832e13f408cdd0ddde6956fd74
Dimitri Shukla <dimitri@dimitri.wifi.local.cmu.edu>
2023-12-09 17:44:09 -0500

    using dynamic=False

--- test.py
+++ test.py
@@ -3,5 +3,5 @@ from ultralytics import YOLO
 
 print("starting")
 model = YOLO("./bin/best.pt")
-model.export(format="ONNX",simplify=True,opset=12)
+model.export(format="ONNX",simplify=True,opset=12,dynamic=False)
 print("done")
