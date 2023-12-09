58d59d612e5ee0acfa5eddfe772acae40cff4dbc
Dimitri Shukla <dimitri@dimitri.wifi.local.cmu.edu>
2023-12-09 17:34:06 -0500

    Using opset=12

--- test.py
+++ test.py
@@ -3,5 +3,5 @@ from ultralytics import YOLO
 
 print("starting")
 model = YOLO("./bin/best.pt")
-model.export(format="ONNX",simplify=True)
+model.export(format="ONNX",simplify=True,opset=12)
 print("done")
