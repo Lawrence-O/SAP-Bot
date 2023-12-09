ffaa534f2cac3272098e5311ef68d4f2c45d585d
Dimitri Shukla <dimitri@dimitri.wifi.local.cmu.edu>
2023-12-09 18:19:35 -0500

    new yolo org

--- test.py
+++ test.py
@@ -2,6 +2,6 @@ from ultralytics import YOLO
 
 
 print("starting")
-model = YOLO("./bin/best.pt")
+model = YOLO("./bin/yolov8_org.pt")
 model.export(format="ONNX",simplify=True,opset=12,dynamic=False)
 print("done")
